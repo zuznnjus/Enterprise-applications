6b83cd55a0a1eff38bf3390f5db22b300e9cb91f
Juszczak <juszczakzuzanna99@gmail.com>
2022-03-21 23:06:47 +0100

    Add JSF exercise

---
+++ JSFLab/src/main/java/view/backing/LoginBean.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package view.backing;
+
+import java.util.ResourceBundle;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlSelectBooleanCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+@Named(value = "loginBean")
+@RequestScoped
+public class LoginBean {
+    
+    private String username;
+    private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton; 
+    
+    public String login() {
+        if (username.equals(password)) {
+            return "success";
+        }
+        else if (username.equals("scott") && password.equals("tiger")) {
+            return loginScott();
+        } else {
+            return "failure";
+        }
+    }
+    
+    public String loginScott() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String message = ResourceBundle
+                .getBundle("ApplicationMessages", context.getViewRoot().getLocale())
+                .getString("validation.scott");
+        context.addMessage(
+                null, 
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null)
+        );
+        
+        return "";
+    }
+    
+    public void activateButton(ValueChangeEvent e) {
+        if (acceptCheckbox.isSelected()) {
+            loginButton.setDisabled(false);
+        } else {
+            loginButton.setDisabled(true);
+        }
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+    
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the acceptCheckbox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    /**
+     * @param acceptCheckbox the acceptCheckbox to set
+     */
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+}
