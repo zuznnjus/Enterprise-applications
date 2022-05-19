eb7bde046aefea43ac791e526abef138eee02231
Zuzanna Juszczak <51724698+zuznnjus@users.noreply.github.com>
2022-05-19 19:34:14 +0200

    Add .net + entityframework exercises

---
+++ MvcNews/MvcNews/Data/NewsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcNews.Data
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) :
+        base(options)
+        { }
+        public DbSet<NewsItem> News { get; set; }
+    }
+
+}
---
+++ MvcNews/MvcNews/Migrations/20220519170537_AddRowVersionMig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcNews.Migrations
+{
+    public partial class AddRowVersionMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(140)",
+                maxLength: 140,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "News",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "News");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(140)",
+                oldMaxLength: 140);
+        }
+    }
+}
