d44ecb5a079f088f963f79edd4c4b0bb81030c7a
Zuzanna Juszczak <51724698+zuznnjus@users.noreply.github.com>
2022-05-19 21:03:05 +0200

    Add .net web api exercises

---
+++ CompanyApi/CompanyApi/Models/DepartmentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Models
+{
+    public class DepartmentDTO
+    {
+        public int DepartmentId { get; set; }
+        public string? Name { get; set; }
+    }
+}
